1328c6a33a8df7e9905133dbe7612147dd6a3262
FloweringFor <3302902667@qq.com>
2026-03-24 16:06:09 +0800

    finish - Problem (learning_rate_schedule): Implement cosine learning rate schedule with warmup

--- cs336_basics/train.py
+++ cs336_basics/train.py
@@ -72,3 +72,11 @@ class AdamW(torch.optim.Optimizer):
         return loss
 
 
+def learning_rate_schedule(t, alpha_max, alpha_min, t_w, t_c):
+    if t < t_w:
+        alpha_t = t / t_w * alpha_max
+    elif t <= t_c:
+        alpha_t = alpha_min + 0.5 * (1 + math.cos((t - t_w) / (t_c - t_w) * math.pi)) * (alpha_max - alpha_min)
+    else:
+        alpha_t = alpha_min
+    return alpha_t
